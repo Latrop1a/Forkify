2f7b857526d10b20559882a05d10053b3ff7e776
Latrop <32779759+Latrop1a@users.noreply.github.com>
2019-03-28 23:17:04 +0100

    Set up dev enviroment with webpack and babel; src and dist folder etc

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,4 @@
-// Global app controller
+import string from "./models/Search";
+import { addSomething, multiplySomething, ID } from "./views/searchView";
+
+console.log(`Using imported func! ${addSomething(ID, 2)}`);
---
+++ src/js/views/searchView.js
@@ -0,0 +1,3 @@
+export const addSomething = (a, b) => a + b;
+export const multiplySomething = (a, b) => a * b;
+export const ID = 23;
